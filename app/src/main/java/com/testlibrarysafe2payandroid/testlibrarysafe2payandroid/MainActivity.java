00579a9c9921d554008e40e3098786d989fe16a4
luisgouvea <luiseduardo.gouvea@gmail.com>
2019-05-23 09:18:02 -0300

    f sdf ds ds

--- app/src/main/java/com/testlibrarysafe2payandroid/testlibrarysafe2payandroid/MainActivity.java
+++ app/src/main/java/com/testlibrarysafe2payandroid/testlibrarysafe2payandroid/MainActivity.java
@@ -3,32 +3,34 @@ package com.testlibrarysafe2payandroid.testlibrarysafe2payandroid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.librarysafe2payapiandroid.librarysafe2payapi.GetTransactionCallback;
+import com.librarysafe2payapiandroid.librarysafe2payapi.ModelTransactionBase;
 import com.librarysafe2payapiandroid.librarysafe2payapi.ResponseAPIModel;
 import com.librarysafe2payapiandroid.librarysafe2payapi.Safe2PayConfig;
-import com.librarysafe2payapiandroid.librarysafe2payapi.SingleSaleAddCallback;
-import com.librarysafe2payapiandroid.librarysafe2payapi.SingleSaleIntegration;
-import com.librarysafe2payapiandroid.librarysafe2payapi.SingleSaleModel;
+import com.librarysafe2payapiandroid.librarysafe2payapi.TransactionIntegration;
 
-public class MainActivity extends AppCompatActivity implements SingleSaleAddCallback {
+public class MainActivity extends AppCompatActivity implements GetTransactionCallback {
 
-    private SingleSaleAddCallback singleSaleAddCallback = this;
+    private GetTransactionCallback getTransactionCallback = this;
+    private String key = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Safe2PayConfig.setKey("48385783758438758375");
+        Safe2PayConfig.setKey(key);
 
-        SingleSaleIntegration.addSingleSale(singleSaleAddCallback, 12.3, 2);
+        TransactionIntegration.getTransaction(getTransactionCallback, 413125);
     }
 
     @Override
-    public void requestSingleSaleAddSuccess(SingleSaleModel authenticationRequest) {
-
+    public void requestGetTransactionSuccess(ModelTransactionBase modelTransactionBase) {
+        String gg = "fff";
     }
 
     @Override
-    public void requestSingleSaleAddFailed(ResponseAPIModel errorResponseAPI) {
-
+    public void requestGetTransactionFailed(ResponseAPIModel errorResponseAPI) {
+        String gg = "fff";
     }
 }
